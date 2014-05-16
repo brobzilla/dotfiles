be8f905ffc79e316c8665a3ec9f65be97a61a5bc
Rob Kennedy <rob.kennedy@videon-central.com>
2014-05-16 16:13:10 -0400

    Add Checks for os

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-
-if test ! $(which rbenv)
-then
-  echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+if [ "$(uname)" == "Darwin" ]; then
+	do_mac
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
 fi
 
-if test ! $(which ruby-build)
-then
-  echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
-fi
+do_mac() {
+	if test ! $(which rbenv)
+	then
+	  echo "  Installing rbenv for you."
+	  brew install rbenv > /tmp/rbenv-install.log
+	fi
+	
+	if test ! $(which ruby-build)
+	then
+	  echo "  Installing ruby-build for you."
+	  brew install ruby-build > /tmp/ruby-build-install.log
+	fi
+}
