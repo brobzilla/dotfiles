f39c8ff1a8050417652ce806ba114ec1fd6d6073
Rob Kennedy <rob.kennedy@videon-central.com>
2014-05-19 10:29:16 -0400

    Messed up the path...derp

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/rkennedy/bin/haste-client/bin/:/home/rkennedy/bin/$DOTFILES/bin:$PATH"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/rkennedy/bin/haste-client/bin/:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
