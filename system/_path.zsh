73225aafb1647ef26af1a035f06eb16592da33e8
Rob Kennedy <rob.kennedy@videon-central.com>
2014-05-19 10:26:54 -0400

    Update default path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-#export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/rkennedy/bin/haste-client/bin/:/home/rkennedy/bin/"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/rkennedy/bin/haste-client/bin/:/home/rkennedy/bin/$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
